2f588d855157bb5c8062c2b592da409a06f84ab2
arjg0707 <113953817+arjg0707@users.noreply.github.com>
2022-12-16 20:39:52 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Project_002;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using static Xamarin.Essentials.Permissions;
+using static Xamarin.Forms.Internals.Profile;
+
+namespace Project_002
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void OnButtonClicked(System.Object sender, System.EventArgs e)
+        {
+            var name = name_.Text;
+            var dateofBirth = date_.Date;
+            var occupation = occupation_.Text;
+            var phone = phone_.Text;
+            var email = email_.Text;
+            var nationality = nationality_.Items[nationality_.SelectedIndex];
+            var level = "";
+            if (RadioBtnBasico.IsChecked) { level = RadioBtnBasico.Value.ToString(); }
+            if (RadioBtnIntermedio.IsChecked) { level = RadioBtnIntermedio.Value.ToString(); }
+            if (RadioBtnAvanzado.IsChecked) { level = RadioBtnAvanzado.Value.ToString(); }
+            if (RadioBtnFluido.IsChecked) { level = RadioBtnFluido.Value.ToString(); }
+            var languages = new ObservableCollection<string>();
+            if (Cpp.IsChecked) { languages.Add("C++"); }
+            if (Csharp.IsChecked) { languages.Add("C#"); }
+            if (Java.IsChecked) { languages.Add("Java"); }
+            if (Python.IsChecked) { languages.Add("Python"); }
+            var aptitudes = aptitudes_.Items[aptitudes_.SelectedIndex];
+            var skills = new ObservableCollection<string>();
+            if (skill_1.IsChecked) { skills.Add("Acondicionamiento fisico"); }
+            if (skill_2.IsChecked) { skills.Add("Creacion de videos deportivos"); }
+            if (skill_3.IsChecked) { skills.Add("Pack office"); }
+            if (skill_4.IsChecked) { skills.Add("Tenis"); }
+            var profile = profile_.Text;
+
+            await Application.Current.MainPage.Navigation.PushAsync(new toShowCV(name, dateofBirth, occupation, phone, email, nationality, level, languages, aptitudes, skills, profile));
+        }
+
+    }
+}
---
+++ toShowCV.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project_002
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class toShowCV : ContentPage
+    {
+        public toShowCV()
+        {
+            InitializeComponent();
+        }
+
+        private string nameContentProperty;
+        private DateTime dateContentProperty;
+        private string occupationContentProperty;
+        private string phoneContentProperty;
+        private string emailContentProperty;
+        private string nationalityContentProperty;
+        private string levelContentProperty;
+        private ObservableCollection<string> languagesContentProperty = new ObservableCollection<string>();
+        private string aptitudesContentProperty;
+        private ObservableCollection<string> skillsContentProperty = new ObservableCollection<string>();
+        private string profileContentProperty;
+
+        public string NameContentProperty
+        {
+            get { return nameContentProperty; }
+            set
+            {
+                nameContentProperty = value;
+                OnPropertyChanged(nameof(NameContentProperty));
+            }
+        }
+        public DateTime DateContentProperty
+        {
+            get { return dateContentProperty; }
+            set
+            {
+                dateContentProperty = value;
+                OnPropertyChanged(nameof(DateContentProperty));
+            }
+        }
+        public string OccupationContentProperty
+        {
+            get { return occupationContentProperty; }
+            set
+            {
+                occupationContentProperty = value;
+                OnPropertyChanged(nameof(OccupationContentProperty));
+            }
+        }
+        public string PhoneContentProperty
+        {
+            get { return phoneContentProperty; }
+            set
+            {
+                phoneContentProperty = value;
+                OnPropertyChanged(nameof(PhoneContentProperty));
+            }
+        }
+        public string EmailContentProperty
+        {
+            get { return emailContentProperty; }
+            set
+            {
+                emailContentProperty = value;
+                OnPropertyChanged(nameof(EmailContentProperty));
+            }
+        }
+        public string NationalityContentProperty
+        {
+            get { return nationalityContentProperty; }
+            set
+            {
+                nationalityContentProperty = value;
+                OnPropertyChanged(nameof(NationalityContentProperty));
+            }
+        }
+        public string LevelContentProperty
+        {
+            get { return levelContentProperty; }
+            set
+            {
+                levelContentProperty = value;
+                OnPropertyChanged(nameof(LevelContentProperty));
+            }
+        }
+        public ObservableCollection<string> LanguagesContentProperty
+        {
+            get { return languagesContentProperty; }
+            set
+            {
+                if(value != null)
+                {
+                    languagesContentProperty = value;
+                    OnPropertyChanged();
+                }          
+            }
+        }
+        public string AptitudesContentProperty
+        {
+            get { return aptitudesContentProperty; }
+            set
+            {
+                aptitudesContentProperty = value;
+                OnPropertyChanged(nameof(AptitudesContentProperty));
+            }
+        }
+        public ObservableCollection<string> SkillsContentProperty
+        {
+            get { return skillsContentProperty; }
+            set
+            {
+                if (value != null)
+                {
+                    skillsContentProperty = value;
+                    OnPropertyChanged();
+                }               
+            }
+        }
+        public string ProfileContentProperty
+        {
+            get { return profileContentProperty; }
+            set
+            {
+                profileContentProperty = value;
+                OnPropertyChanged(nameof(ProfileContentProperty));
+            }
+        }
+
+        public toShowCV(string nameContentProperty_, DateTime dateContentProperty_, string occupationContentProperty_, string phoneContentProperty_, string emailContentProperty_, string nationalityContentProperty_, string levelContentProperty_, ObservableCollection<string> languagesContentProperty_, string aptitudesContentProperty_,  ObservableCollection<string> skillsContentProperty_, string profileContentProperty_)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            NameContentProperty = nameContentProperty_;
+            DateContentProperty = dateContentProperty_;
+            OccupationContentProperty = occupationContentProperty_;
+            PhoneContentProperty = phoneContentProperty_;
+            EmailContentProperty = emailContentProperty_;
+            NationalityContentProperty = nationalityContentProperty_;
+            LevelContentProperty = levelContentProperty_;
+            LanguagesContentProperty = new ObservableCollection<string>(languagesContentProperty_);
+            AptitudesContentProperty = aptitudesContentProperty_;
+            SkillsContentProperty = new ObservableCollection<string>(skillsContentProperty_);
+            ProfileContentProperty = profileContentProperty_;
+        }
+    }
+}
